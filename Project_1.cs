404247ad98286661ba250cf0a2f59bcc6d5e5bf4
tdrier1 <tdrier1@gmail.com>
2017-03-30 17:42:48 -0400

    Commiting first Project

---
+++ Project_1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Project_1
+{
+    class Project_1
+    {   
+        //Main method and entry point for the program execution
+        static void Main(string[] args)
+        {
+            //Here is were the variables are defined
+            int rem;
+            int rev = 0;
+
+
+            //Prompt for user to enter string value and convert to an integer
+            Console.Write("Please Enter Your Number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            //While loop used to perform the number reversal
+            while (num > 0)
+            {
+                rem = (num % 10);
+                rev = (rev * 10) + rem;
+                num = (num / 10);
+            }
+
+            Console.WriteLine("\n");
+
+            //Comand returning string value and input number reveresed 
+            Console.WriteLine("Your Number Reversed: " + rev);
+
+            Console.ReadKey();
+
+        }
+    }
+}
